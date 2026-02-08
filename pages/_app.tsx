3441519c4c9795f8b452e32c9612b7fd8f845a82
Byby1337 <svit.andersen@gmail.com>
2026-02-08 17:49:08 +0300

    update _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <PrivyProvider appId="cmkn4ut3u00kijp0cvfbqbk6l"
     config={{
       loginMethods: ['wallet'],
-      embeddedWallets: { createOnLogin: 'users-without-wallets',
+      embeddedWallets: { 
       },
     }}>
       <Component {...pageProps} />
